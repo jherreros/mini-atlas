93f0854edfe717466bf3caeaf3020edaa511551b
Juan Herreros Elorza <11259554+jherreros@users.noreply.github.com>
2025-07-06 11:21:23 +0200

    fix CNI config

--- 2-addons/install-addons.sh
+++ 2-addons/install-addons.sh
@@ -2,6 +2,18 @@
 
 set -o errexit
 
+CILIUM_VERSION="1.17.5"
+
+# Cilium
+helm repo add cilium https://helm.cilium.io/
+helm install cilium cilium/cilium --version ${CILIUM_VERSION} \
+   --namespace kube-system \
+   --set kubeProxyReplacement=true \
+   --set image.pullPolicy=IfNotPresent \
+   --set ipam.mode=kubernetes
+
+cilium status --wait
+
 # This script installs FluxCD.
 
 if ! command -v flux &> /dev/null
@@ -22,6 +34,6 @@ then
     flux install
     kubectl apply -f flux/
 else
-    echo "FluxCD already bootstrapped. Reconciling..."
+    echo "FluxCD already installed. Reconciling..."
     flux reconcile kustomization flux-system --with-source
 fi
